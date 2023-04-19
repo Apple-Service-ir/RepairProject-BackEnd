83cc8c70f3b38dc552521047ecd417a2f17b42ab
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-19 22:25:29 +0330

    Responses updated

--- Back-End/controllers/registerController.js
+++ Back-End/controllers/registerController.js
@@ -3,6 +3,9 @@ const User = require("../models/User");
 const post = async (req, res) => {
   const [firstName, lastName, city, phone, password] = req.body; // Data come from request
 
+  const isPhoneUsed = await User.findOne({ where: { phone } });
+  if (isPhoneUsed) return res.send({ ok: false, err: "phone number used" });
+
   // Create new user
   await User.create({
     firstName,
@@ -14,11 +17,11 @@ const post = async (req, res) => {
     password: await User.encryptPassword(password),
   })
     .then(() => {
-      res.send(true); // Successfully created
+      res.send({ ok: true }); // Successfully created
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).send(false); // Can't create
+      res.status(500).send({ ok: false, err: "can't create user" }); // Can't create
     });
 };
 
