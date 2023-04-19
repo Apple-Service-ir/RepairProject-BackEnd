0e1f1025e3ddbf4268beaa66cca44990ae1b6d1f
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-19 17:29:09 +0330

    Unused file deleted | comment added

--- Back-End/helpers/defaultChecker.js
+++ Back-End/helpers/defaultChecker.js
@@ -3,16 +3,19 @@ const fs = require("fs");
 
 const defaultChecker = async () => {
   console.log("Reading Models directory.");
+  // Read models and create table for each model which is not exist
   fs.readdirSync("./models").forEach((model) => {
     console.log(`Registering model ${model.split(".")[0]}`);
     const requiredModel = require(`../models/${model}`);
-    requiredModel.sync();
+    requiredModel.sync(); // Check and create table if it's not exist
   });
 
+  // Check for default admin user
   console.log("Checking default user");
   const requireUser = require("../models/User");
   await requireUser.findAll().then(async (result) => {
     if (!result || !result[0]) {
+      // No admin
       console.log(
         `--------------------\nDefault user created\n\tUsername : ${config.defaultUser.username}\n\tPassword : ${config.defaultUser.password}\n--------------------`
       );
@@ -22,7 +25,7 @@ const defaultChecker = async () => {
           config.defaultUser.password
         ),
         userRank: config.defaultUser.userRank,
-      });
+      }); // Insert default user using config file
     }
   });
 };
