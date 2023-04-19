70d4ad83aaf502ddc8f822140050ef31d6aacd37
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-19 17:06:48 +0330

    Auth file replaced with new one

---
+++ Back-End/helpers/jwtAuth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const config = require("../configs/config.json");
+
+const isUserLoggedIn = (req, res, next) => {
+  try {
+    // Trying for decode token if it's exist
+    const user = jwt.verify(token, config.app.secret);
+    req.user = user; // Set user informations (anything is in User model) into request
+    next(); // Going
+  } catch (error) {
+    res.send({ isUserLoggedIn: false }); // User is not logged in
+  }
+};
+
+const isUserNotLoggedIn = (req, res, next) => {
+  try {
+    // Trying for decode token if it's exist
+    // ! Becuase it's "notLoggedIn", if decode token is success proccess will broke and send false
+    jwt.verify(token, config.app.secret);
+    res.send({ isUserNotLoggedIn: false });
+  } catch (error) {
+    next();
+  }
+};
+
+module.exports{
+    isUserLoggedIn,
+    isUserNotLoggedIn
+}
