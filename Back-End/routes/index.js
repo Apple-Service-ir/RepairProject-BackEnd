f851c956a7d5b5c17b4bd263528afc8a8fc60bc5
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-19 17:08:22 +0330

    Routes

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const { isAdmin, isLoggedIn } = require("../helpers/auth");
 const Router = express.Router();
 
-const singleBlogController = require("../controllers/singleBlogController");
-Router.get("/blogs/:id", singleBlogController.get);
-Router.post("/blogs/:id", isLoggedIn, isAdmin, singleBlogController.post);
-
-const blogsController = require("../controllers/blogsController");
-Router.get("/blogs", blogsController.get);
+const registerController = require("../models/User");
+Router.post("/register", registerController.post);
 
 module.exports = Router;
