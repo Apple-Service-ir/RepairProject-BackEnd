690156baa70dcbad9f6c174db176ea7724b7a6e5
Shayan NasrAbadi <williams.ca001@gmail.com>
2023-04-19 17:20:48 +0330

    Register API completed

--- Back-End/controllers/registerController.js
+++ Back-End/controllers/registerController.js
@@ -1,4 +1,24 @@
-const post = (req, res) => {};
+const User = require("../models/User");
+
+const post = async (req, res) => {
+  const [firstName, lastName, city, phone, password] = req.body;
+
+  await User.create({
+    firstName,
+    lastName,
+    role: "user",
+    city,
+    phone,
+    profile: req.file.filename,
+    password: await User.encryptPassword(password),
+  })
+    .then(() => {
+      res.send(true);
+    })
+    .catch(() => {
+      res.send(false);
+    });
+};
 
 module.exports = {
   post,
