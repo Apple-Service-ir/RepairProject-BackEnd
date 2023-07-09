b74432b7e6bb81249676cf4cfe46b7c2b9e33159
Wil1i <williams.ca001@gmail.com>
2023-07-08 23:41:19 -0700

    Transactions for order added to response

--- controllers/orders/logController.js
+++ controllers/orders/logController.js
@@ -1,7 +1,7 @@
 const Order = require("../../models/Order");
 
 const get = async (req, res) => {
-  res.json({ ok: true, orders: await Order.findAll({ where: { userId: req.user.id }, order: [['id', 'DESC']], include: ['repairman', 'user'] }) })
+  res.json({ ok: true, orders: await Order.findAll({ where: { userId: req.user.id }, order: [['id', 'DESC']], include: ['repairman', 'user', 'transactions'] }) })
 };
 
 module.exports = {
