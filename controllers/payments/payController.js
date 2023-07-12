161f9bffb81a24910f07c143a233bafea9d92760
Wil1i <williams.ca001@gmail.com>
2023-07-12 01:51:30 -0700

    Pay controller fixed and completed

--- controllers/payments/payController.js
+++ controllers/payments/payController.js
@@ -1,4 +1,3 @@
-const Transaction = require("../../models/Transaction")
 const zarinpal = require("../../helpers/zarinpal")
 const Order = require("../../models/Order")
 const config = require("../../configs/config.json")
@@ -6,20 +5,24 @@ const config = require("../../configs/config.json")
 const post = async (req, res) => {
     if (!req.body.id) return res.status(401).json({ ok: false, err: "parameters not found [id]" })
 
-    const findTransaction = await Transaction.findByPk(req.body.id)
-    if (!findTransaction) return res.status(401).json({ ok: false, err: "invalid transaction id" })
-
-    const findOrder = await Order.findByPk(findTransaction.orderId, { include: ['user'] })
+    const findOrder = await Order.findByPk(req.body.id, { include: ['user', 'transactions'] })
     if (!findOrder) return res.status(401).json({ ok: false, err: "invalid order id" })
 
+    const filtered = findOrder.transactions.filter(s => s.status == "pending")
+    if (!filtered[0]) return res.json({ ok: false, err: "have not pending transactions" })
+    const amount = filtered.map(s => s.price).reduce((prev, current) => prev + current)
+
     zarinpal.PaymentRequest({
-        Amount: findTransaction.price + "",
-        CallbackURL: `${config.mainURL}/payment/done`,
+        Amount: amount,
+        CallbackURL: `http://192.168.1.138:5173/payments/done`,
         Description: `تعمیر ${findOrder.phoneName}`,
         Email: "",
         Mobile: findOrder.user.phone
-    }).then(res => {
-        if (response.status === 100) res.json({ ok: true, url: response.url })
+    }).then(async response => {
+        if (response.status === 100) {
+            await findOrder.update({ authority: response.authority })
+            res.json({ ok: true, url: response.url })
+        }
     }).catch(err => {
         res.json({ ok: false, err })
     })
