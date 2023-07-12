f100f0c506e29b48d696d03f77cd7b7521cb5920
Wil1i <williams.ca001@gmail.com>
2023-07-12 01:50:54 -0700

    Sandbox added to zarinpal

--- helpers/zarinpal.js
+++ helpers/zarinpal.js
@@ -1,6 +1,6 @@
 const ZarinpalCheckout = require("zarinpal-checkout")
 const config = require("../configs/config.json")
 
-const zarinpal = ZarinpalCheckout.create(config.merchentId)
+const zarinpal = ZarinpalCheckout.create(config.merchentId, true)
 
 module.exports = zarinpal
