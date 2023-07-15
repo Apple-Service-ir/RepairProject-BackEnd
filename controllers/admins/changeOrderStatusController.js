b5bb3504969e704bc04915b31ebe7f699733dab5
Wil1i <williams.ca001@gmail.com>
2023-07-15 03:06:17 -0700

    Bug fix

--- controllers/admins/changeOrderStatusController.js
+++ controllers/admins/changeOrderStatusController.js
@@ -1,22 +1,33 @@
 const Order = require("../../models/Order")
+const Transaction = require("../../models/Transaction")
 const config = require("../../configs/config.json")
 const sms = require("../../helpers/sms")
 
 const post = async (req, res) => {
     if (!req.body.id || !req.body.status) return res.status(400).json({ ok: false, err: "parameters undefined [id, status]" })
 
-    const findOrder = await Order.findByPk(req.body.id, { include: ['user'] })
+    const findOrder = await Order.findByPk(req.body.id, { include: ['user', 'repairman', 'transactions'] })
     if (!findOrder) return res.status(400).json({ ok: false, err: "order undefined" })
+
+    if (findOrder.status == req.body.status) return res.status(401).json({ ok: false, err: "وضعیت سفارش تکراری می‌باشد." })
 
     if (findOrder.userId == req.user.id) return res.status(403).json({ ok: false, err: "شما دسترسی برای ایجاد تغییر در سفارش خود ندارید." })
 
-    if (!['pending', 'working', 'done', 'cancelled'].includes(req.body.status)) return res.status(400).json({ ok: false, err: "bad status" })
+    if (!['pending', 'working', 'done', 'cancelled', 'payment-working', 'payment-done'].includes(req.body.status)) return res.status(400).json({ ok: false, err: "bad status" })
 
-    if (findOrder.status == "pending" && req.body.status == "working") sms.send(`${findOrder.user.firstName};${findOrder.id};`, findOrder.user.phone, config.smsOrder);
+    if (findOrder.status == "pending" && (req.body.status == "working" || req.body.status == "payment-working")) sms.send(`${findOrder.user.firstName};${findOrder.id};`, findOrder.user.phone, config.smsOrder);
 
     await findOrder.update({ status: req.body.status, adminMessage: req.body.adminMessage || null })
 
-    res.json({ ok: true })
+    if (req.body.status.startsWith("payment-")) {
+        await Transaction.create({
+            price: req.body.price,
+            orderId: req.body.id,
+            status: "pending"
+        })
+    }
+
+    res.json({ ok: true, order: findOrder })
 }
 
 module.exports = {
